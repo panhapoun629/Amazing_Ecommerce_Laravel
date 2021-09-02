c0e125d98b12c6b8f9644bf7963e4b920a11c0dd
panhapoun <tiengpanhapoun629@gmail.com>
2021-09-02 11:53:05 +0700

    header, footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <footer class="bg-success">
+    <div class="container">
+      <div class="row">
+            <div class="col-md-3 col-sm-6">
+              <h3>About Us</h3>
+              <p>We provide you the best solutions and creative ideas with HTML, CSS, Bootstrap, Javascript, Jquery technologies, and SEO.</p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#">Link First</a></li>
+                    <li><a href="#">Link Second</a></li>
+                    <li><a href="#">Link Third</a></li>
+                    <li><a href="#">Link Fourth</a></li>
+                    <li><a href="#">Link Fifth</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h3>Our Services</h3>
+                <ul>
+                    <li><a href="#">Link First</a></li>
+                    <li><a href="#">Link Second</a></li>
+                    <li><a href="#">Link Third</a></li>
+                    <li><a href="#">Link Fourth</a></li>
+                    <li><a href="#">Link Fifth</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h3>Download App</h3>
+                <div class="follow-us">
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+                        <li><a href="#"><i class="fab fa-linkedin-in"></i> Linkedin</a></li>
+                    </ul>
+                </div>
+            </div>                
+      </div>
+    </div>
+
+    <!-- Copyright Text -->
+    <div class="copyright-txt">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center">markuptag Copyright 2020. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
